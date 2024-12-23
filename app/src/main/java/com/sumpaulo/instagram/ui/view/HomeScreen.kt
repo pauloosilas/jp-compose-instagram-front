d2964cf1e95f5efc50d880d3bbc96865d74c341f
pauloosilas <paulo3356@gmail.com>
2024-12-23 13:42:09 -0300

    add list stories

--- app/src/main/java/com/sumpaulo/instagram/ui/view/HomeScreen.kt
+++ app/src/main/java/com/sumpaulo/instagram/ui/view/HomeScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,6 +18,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.sumpaulo.instagram.R
 import com.sumpaulo.instagram.data.Story
+import com.sumpaulo.instagram.ui.theme.spacingMedium
 
 @Composable
 fun HomeScreen() {
@@ -25,7 +28,56 @@ fun HomeScreen() {
             .background(MaterialTheme.colorScheme.background)
     ) {
         InstagramToolbar()
-        StoryItem(story = Story(userNickName = "Jane Doe", userAvatar = "https://static.vecteezy.com/system/resources/thumbnails/008/846/297/small_2x/cute-boy-avatar-png.png"))
+
+        val stories = listOf<Story>(
+            Story(
+                userNickName = "janedoe",
+                userAvatar = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4IaDTwdkqf3m-2RqnUAnR3usA1Hk6usf0yA&s"
+            ),
+            Story(
+                userNickName = "juremagpt",
+                userAvatar = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4IaDTwdkqf3m-2RqnUAnR3usA1Hk6usf0yA&s"
+            ),
+            Story(
+                userNickName = "devdoido",
+                userAvatar = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4IaDTwdkqf3m-2RqnUAnR3usA1Hk6usf0yA&s"
+            ),
+            Story(
+                userNickName = "kkkkkkkkburrao",
+                userAvatar = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4IaDTwdkqf3m-2RqnUAnR3usA1Hk6usf0yA&s"
+            ),
+            Story(
+                userNickName = "janedoe",
+                userAvatar = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4IaDTwdkqf3m-2RqnUAnR3usA1Hk6usf0yA&s"
+            ),
+            Story(
+                userNickName = "juremagpt",
+                userAvatar = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4IaDTwdkqf3m-2RqnUAnR3usA1Hk6usf0yA&s"
+            ),
+            Story(
+                userNickName = "devdoido",
+                userAvatar = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4IaDTwdkqf3m-2RqnUAnR3usA1Hk6usf0yA&s"
+            ),
+            Story(
+                userNickName = "kkkkkkkkburrao",
+                userAvatar = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4IaDTwdkqf3m-2RqnUAnR3usA1Hk6usf0yA&s"
+            )
+        )
+
+        StoryList(stories = stories)
+    }
+}
+
+@Composable
+fun StoryList(stories: List<Story>) {
+    LazyRow(
+        modifier = Modifier
+            .padding(top = spacingMedium)
+    )
+    {
+        itemsIndexed(stories) { _, item ->
+            StoryItem(story = item)
+        }
     }
 }
 
--- app/src/main/java/com/sumpaulo/instagram/ui/view/StoryItem.kt
+++ app/src/main/java/com/sumpaulo/instagram/ui/view/StoryItem.kt
@@ -1,27 +1,59 @@
 package com.sumpaulo.instagram.ui.view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.sumpaulo.instagram.data.Story
 import com.sumpaulo.instagram.ui.theme.spacingSmall
+import com.sumpaulo.instagram.R
+import com.sumpaulo.instagram.ui.theme.storyCircleColor
 
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun StoryItem(story: Story) {
+
+    val avatarContentDesc = stringResource(id = R.string.content_description_story, story.userNickName)
+
     Column(
         modifier = Modifier
             .padding(spacingSmall)
             .background(MaterialTheme.colorScheme.background)
     ) {
-        GlideImage(model = story.userAvatar,
-            contentDescription = "")
+        GlideImage(
+            modifier = Modifier
+                .size(64.dp)
+                .align(Alignment.CenterHorizontally)
+                .fillMaxSize()
+                .clip(CircleShape)
+                .border(2.dp, storyCircleColor, CircleShape),
+            model = story.userAvatar,
+            contentDescription = avatarContentDesc)
+
+        Text(text = story.userNickName,
+            modifier = Modifier
+            .size(width = 72.dp, height = 24.dp),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            textAlign = TextAlign.Center
+            )
     }
 }
 
